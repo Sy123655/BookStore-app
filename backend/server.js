2595bd0fc679070bf19aefd28951d9c35589808a
Sy123655 <58252162+Sy123655@users.noreply.github.com>
2021-06-07 10:01:40 +0700

    dsa
    asdas

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,9 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
     useCreateIndex: true,
 });
 app.use('/api/uploads', uploadRouter);
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter)
@@ -26,9 +29,9 @@ app.use('/api/config/paypal', (req, res) =>{
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.get('/', (req, res) =>{
-    res.send('Server is ready');
-});
+// app.get('/', (req, res) =>{
+//     res.send('Server is ready');
+// });
 
 app.use((err, req, res, next) =>{
     res.status(500).send({ message: err.message })
